b8d45710212e56957ecc9aa9f4f4f588a495e6cf
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2021-01-28 11:44:34 -0600

    git setup command

---
+++ setup-git.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh -f
+set -euo pipefail
+
+echo "Setting default branch to master globally"
+git config --global init.defaultBranch master
+
+echo "Setting default Global Name to my name"
+git config --global user.name "Nicholas Eastman"
+
+echo "Setting default Global Email address to my maintainer email"
+git config --global user.email "29488820+ironman820@users.noreply.github.com"
+
+echo "Done!"
