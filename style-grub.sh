0322c9bd7c69554558693d37293098d88d86e313
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2021-01-28 16:25:38 -0600

    Grub style restore

--- style-grub.sh
+++ style-grub.sh
@@ -11,6 +11,6 @@ fi
 
 paru -Sy --noconfirm grub2-theme-archlinux
 
-cp $MYDIR/.backups/root/etc/default/grub /etc/default/
+sudo cp $MYDIR/.backups/root/etc/default/grub /etc/default/
 
-grub-mkconfig -o /boot/grub/grub.cfg
+sudo grub-mkconfig -o /boot/grub/grub.cfg
