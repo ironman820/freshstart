1b7cad29ab4e0240464b4627d43235ee376e18de
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2021-04-30 08:52:47 -0500

    Removed old emacs links and cleaned up restore

--- restore.sh
+++ restore.sh
@@ -10,7 +10,7 @@ then
     $mydir/setup-aur-helper.sh
 fi
 
-paru -S --needed $(grep --invert-match "glocom" $mydir/.backups/pkglst.txt)
+paru -Syu --needed $(grep --invert-match "glocom" $mydir/.backups/pkglst.txt)
 
 git pull origin master
 
@@ -20,24 +20,26 @@ then
     ~/.emacs.d/bin/doom install
 fi
 
-print "Restoring XMonad Config"
+echo "Restoring XMonad Config"
 cp -rf $mydir/.backups/bin $HOME/
 cp -rf $mydir/.backups/.xmonad $HOME/
 cp -rf $mydir/.backups/.config $HOME/
 cp -rf $mydir/.backups/.doom.d $HOME/
 cp -rf $mydir/.backups/.putty $HOME/
-print "Restoring Application Launchers"
+echo "Restoring Application Launchers"
 cp -rf $mydir/.backups/.local/share/applications $HOME/.local/share/
 
-print "Restoring GRUB config"
+echo "Restoring GRUB config"
 sudo cp $mydir/.backups/root/etc/default/grub /etc/default/
 sudo cp $mydir/.backups/root/etc/grub.d/40_custom /etc/grub.d/
 
-print "Restoring LightDM settings"
+echo "Restoring LightDM settings"
 sudo cp $mydir/.backups/root/etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/
 
-print "Restoring httpd config"
+echo "Restoring httpd config"
 sudo cp $mydir/.backups/root/etc/httpd/conf/extra/httpd-vhosts.conf /etc/httpd/conf/extra/
+
+cd $HOME
 
 cd .xmonad
 xmonad --recompile
